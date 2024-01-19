490e9696fada15267c02135bdba719ceed279f0d
nmonteleone1 <61724985+nmonteleone1@users.noreply.github.com>
2024-01-19 15:33:06 +1100

    Added local storage to save attended and leave dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Calendar from './calendar/Calendar'
 import { getConcatMonthYear } from './common';
 import Stats from './Stats';
 
 
 function App() {
-  const [datesAttended, setDatesAttended] = useState(new Map<string, Array<number>>())
+  const [datesAttended, setDatesAttended] = useState(new Map<string, Array<number>>(Object.entries(JSON.parse(localStorage.getItem('attended') ?? '{}'))));
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [leaveDates, setLeaveDates] = useState(new Map<string, Array<number>>())
+  const [leaveDates, setLeaveDates] = useState(new Map<string, Array<number>>(Object.entries(JSON.parse(localStorage.getItem('leaved') ?? '{}'))));
   const disableDefaultRightClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => event.preventDefault();
+  
+  useEffect(() => {
+    console.log('saving attended and leave');
+    localStorage.setItem('attended', JSON.stringify(Object.fromEntries(datesAttended)));
+    localStorage.setItem('leaved', JSON.stringify(Object.fromEntries(leaveDates)));
+  }, [datesAttended, leaveDates]);
   
   return (
     <div className='flex flex-col h-screen' onContextMenu={disableDefaultRightClick}>
